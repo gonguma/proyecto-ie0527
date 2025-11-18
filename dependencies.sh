80b3e7db159fcdbb89435797f96da415af7d2968
Gonzalo <gonzalo.ie.ucr@gmail.com>
2025-11-18 17:08:18 -0600

    Modificando dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,9 @@
 # Virtual environment para Python
-python3 -m venv .venv
-source .venv/bin/activate
+# python3 -m venv .venv
+# source .venv/bin/activate
 
-# Librería C de pigpio
-sudo apt-get install pigpio
+# Librería C de pigpio / Solo dentro de la Raspberry Pi
+# sudo apt-get install pigpio
 
 # Librería RF24 C++ (desde GitHub)
 git clone https://github.com/nRF24/RF24.git
